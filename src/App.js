c114fbac31d6d7b3e811db459501a057658c2674
StaceyWhitmore <staceywhitmore@gmail.com>
2019-01-03 20:29:53 -0700

    added basic example data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FlashcardApp from 'react-flashcard-app';
+const data = 
+{
+  id: 1,
+  name: 'Example Deck',
+  cards: [
+    {
+      id: 1,
+      front: 'Lorem',
+      back: 'dolor',
+    },
+    {
+      id: 2,
+      front: 'sit',
+      back: 'amet',
+    },
+    {
+      id: 3,
+      front: 'consetetur',
+      back: 'sadipscing',
+    },
+    {
+      id: 4,
+      front: 'sed',
+      back: 'diam',
+    },
+  ],
+};
+
+
 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <FlashcardApp data={data} />
       </div>
     );
+
   }
 }
 
 export default App;
+
+/*
+<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header>
+
+
+*/
